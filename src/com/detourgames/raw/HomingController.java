a50febcbe0b3f4e052aa50fe8f09bd85181daac1
Aaron <aaronrmm@gmail.com>
2012-10-20 14:47:36 -0400

    Homing projectiles dodge hero projectiles

--- src/com/detourgames/raw/HomingController.java
+++ src/com/detourgames/raw/HomingController.java
@@ -3,6 +3,7 @@ package com.detourgames.raw;
 import java.util.Iterator;
 
 import com.badlogic.gdx.physics.box2d.Body;
+import com.detourgames.raw.game.HeroProjectile;
 import com.detourgames.raw.steering.Steerer;
 
 public class HomingController extends ControllerComponent{
@@ -18,16 +19,24 @@ public class HomingController extends ControllerComponent{
 	public void update(StateComponent state, PhysicsComponent physics){
 		Iterator<Body> bodies=GameManager.getGameManager().getLevel().getWorld().getBodies();
 		Body toFlee=null;
-		while(true)
+		while(bodies.hasNext())
 		{
 			Body body = bodies.next();
-			if(body.getPosition().dst(physics.getPosition())<5)
+			Object o = body.getUserData();
+			if(o instanceof PhysicsComponent)
 			{
-				toFlee=body;
-				break;
+				Sprite parent = ((PhysicsComponent) o).getParentSprite();
+				if(parent instanceof HeroProjectile)
+				{
+					if(body.getPosition().dst(physics.getPosition())<5)
+					{
+						toFlee=body;
+						break;
+					}
+				}
 			}
 		}
-		if(true)//toFlee==null)
+		if(toFlee==null)
 			physics.getBody().applyForceToCenter(Steerer.Persue(physics, target).nor().mul(turningRate));
 		else
 			physics.getBody().applyForceToCenter(Steerer.Flee(physics, toFlee).nor().mul(turningRate));
