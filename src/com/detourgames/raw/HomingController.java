e06a6812418507c92f7350894cab9f584b576c40
Clayton Langford <amplify91@gmail.com>
2012-10-20 03:08:07 -0400

    Just being kind of drunk and very OCD. Cleaned up code, changed nothing.

--- src/com/detourgames/raw/HomingController.java
+++ src/com/detourgames/raw/HomingController.java
@@ -7,29 +7,29 @@ import com.detourgames.raw.steering.Steerer;
 
 public class HomingController extends ControllerComponent{
 
-	PhysicsComponent target;
-	float turningRate;
-	public HomingController(PhysicsComponent target,float turningRate)
-	{
-		this.turningRate=turningRate;
-		this.target=target;
+	Sprite mTarget;
+	float mTurningRate;
+	
+	public HomingController(Sprite target, float turningRate){
+		mTurningRate = turningRate;
+		mTarget = target;
 	}
+	
 	@Override
 	public void update(StateComponent state, PhysicsComponent physics){
 		Iterator<Body> bodies=GameManager.getGameManager().getLevel().getWorld().getBodies();
 		Body toFlee=null;
-		while(true)
-		{
+		while(true){
 			Body body = bodies.next();
-			if(body.getPosition().dst(physics.getPosition())<5)
-			{
+			if(body.getPosition().dst(physics.getPosition())<5){
 				toFlee=body;
 				break;
 			}
 		}
-		if(true)//toFlee==null)
-			physics.getBody().applyForceToCenter(Steerer.Persue(physics, target).nor().mul(turningRate));
-		else
-			physics.getBody().applyForceToCenter(Steerer.Flee(physics, toFlee).nor().mul(turningRate));
+		if(true){//toFlee==null)
+			physics.getBody().applyForceToCenter(Steerer.persue(physics.getParentSprite(), mTarget).nor().mul(mTurningRate));
+		}else{
+			physics.getBody().applyForceToCenter(Steerer.flee(physics.getParentSprite(), toFlee).nor().mul(mTurningRate));
+		}
 	}
 }
